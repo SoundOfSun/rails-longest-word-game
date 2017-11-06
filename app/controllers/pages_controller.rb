65b969f0799313e9d78cb74ede491ec53062c143
Cecile Alfred <cecile.alfredx@gmail.com>
2017-11-06 12:52:55 +0000

    Added comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,17 +7,22 @@ require 'json'
 class PagesController < ApplicationController
   # public methods associated to a route
   def game
+    # 1. Generate a grid of 9 random letters
+    # 2. Join the result array
     @grid = generate_grid(9).join(" ")
+    # 3. Get the starting time on the game page
     @start_time = Time.now
-
   end
 
   # raise shows an error & brings a console
   def score
+    # 1. Get start time from /game page
+    @start_time = Time.parse(params[:start_time])
+    # 2. Get end time from /score page
     @end_time = Time.now
+    # 3. Get user's answer from form (input's name)
     @answer = params[:user_shot]
     @grid = params[:grid]
-    @start_time = Time.parse(params[:start_time])
     result = {}
     if english_word?(@answer)
       if matches?(@answer.upcase, @grid)
@@ -40,32 +45,39 @@ class PagesController < ApplicationController
   end
 
   private
-  # help principal methods
-  # generate a random grid, allows duplicates of numbers
+  # PRIVATE METHODS help principal methods
   def generate_grid(grid_size)
-    # TODO: generate random grid of letters
+    # TODO: generate random grid of letters, allows duplicate of letters
+    # 1. Create new array of 9 elements
+    # 2. In the range A to Z for each element of the array, take a sample
+    # (so by default, it allows duplicates)
     grid = Array.new(grid_size) { ("A".."Z").to_a.sample }
     return grid
   end
 
-  # check attemps matches the grid letters
   def matches?(attempt, grid)
-    # condition booleans user_attempt and generated grid
+    # TODO: check that the attempt matches the grid letters
+    # 1. For an attempt, check each characters with .all?
+    # 2. Compare the letters in attempt to equal up to max grid's letters
     attempt.chars.all? { |letter| attempt.count(letter) <= grid.count(letter) }
   end
 
   # check attempt is a real word : english_word? (API)
   def english_word?(word)
+    # TODO: use API to check that an attempt is a real word
+    # 1. Interpolate user's word in the url path
     url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    # 2. Read the content of url
     serialized_word = open(url).read
+    # 3. parse JSON
     word = JSON.parse(serialized_word)
-    return word["found"] == true
-    # boolean
+    # 4. Boolean (when you open the url, 1st key is :found : true/false)
+    return word["found"]
   end
 
-  # find the time it took for answering
   def total_time(start_time, end_time)
-    # time new - time now
+    # TODO: find the time it took for answering
+    # The calculation is : time new - time now
     total_time = end_time - start_time
     return total_time
   end
